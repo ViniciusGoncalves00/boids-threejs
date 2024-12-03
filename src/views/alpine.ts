2681301d363e144efdd4c7c9de09785eb2f23afc
Vinicius <viniciusgoncalves2000@hotmail.com>
2024-12-02 21:48:19 -0300

    New UI

--- src/views/alpine.ts
+++ src/views/alpine.ts
@@ -1,9 +1,21 @@
 import Alpine from "alpinejs";
 
-Alpine.data("domainmenu", () =>
+Alpine.data("options", () =>
     (
         {
             open : false,
+        }
+    )
+);
+
+Alpine.data("slider",({ label = "size", min = 0, max = 10, step = 1, value = 5 }) =>
+    (
+        {
+            label,
+            min,
+            max,
+            step,
+            value,
         }
     )
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,20 @@ module.exports =
   {
     extend:
     {
+      fontFamily: {
+        Montserrat: ["Montserrat"],
+        RobotoCondensed: ["Roboto Condensed"],
+      },
       dropShadow:
       {
-        "custom-shadow-1": "4px 4px 4px rgb(0 0 0 / 0.2)",
+        "custom-shadow-1": "0px 0px 4px rgb(0 0 0 / 0.10)",
         "custom-shadow-2": "4px 4px 4px rgb(0 0 0 / 0.05)"
       },
       colors:
       {
         primary: "rgba(var(--primary))",
         primary_highlight: "rgba(var(--primary_highlight))",
+        secondary: "rgba(var(--secondary))",
     
         primary_text: "rgba(var(--primary_text))",
         primary_highlight_text:"rgba(var(--primary_highlight_text))",

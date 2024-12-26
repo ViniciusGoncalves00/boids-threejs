8ab983b43b3eb228ae8a0f020b9e7d59786ef951
Vinicius <viniciusgoncalves2000@hotmail.com>
2024-12-26 13:49:54 -0300

    Renamed behavior section to boids

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, re_path
 from .views import Index
 from .views.domain import Domain
-from .views.behavior import Behavior
+from .views.boids import Boids
 from .views.simulations import SimulationsView
 
 
 urlpatterns = [
     path('', Index.Index.as_view(), name='index'),
     re_path(r'^domain/(?P<simulation_id>\d+)/?$', Domain.as_view(), name='domain'),
-    re_path(r'^behavior/(?P<simulation_id>\d+)/?$', Behavior.as_view(), name='behavior'),
+    re_path(r'^boids/(?P<simulation_id>\d+)/?$', Boids.as_view(), name='boids'),
     re_path(r'^simulation/(?P<simulation_id>\d+)/?$', SimulationsView.as_view(), name='simulation'),
     re_path(r'^simulations/?$', SimulationsView.as_view(), name='simulations'),
 ]
--- project/app/views/boids.py
+++ project/app/views/boids.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from .base_view import BaseView
 from ..models.simulation import Simulation
 
-class Behavior(BaseView):
-    template = "sections/behavior.html"
+class Boids(BaseView):
+    template = "sections/boids.html"
     
     def get(self, request: HttpRequest, simulation_id) -> HttpResponse:
         try:
@@ -13,5 +13,5 @@ class Behavior(BaseView):
             context = {"simulation": simulation.serialize()}
             
             return render(request, self.template, context)
-        except Behavior.DoesNotExist:
+        except Boids.DoesNotExist:
             return render(request, self.template_not_found)
